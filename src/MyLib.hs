8b682154ac94288f6a43f595411f9397bfa51ce6
0xpantera <0xpantera@proton.me>
2024-05-28 17:03:48 -0600

    strip trailing slashes from path

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,22 @@
-module MyLib (someFunc) where
+{-# LANGUAGE TypeApplications #-}
+module MyLib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Exception (IOException, handle)
+import Control.Monad (join, void, when)
+import Data.Foldable (for_)
+import Data.IORef (modifyIORef, newIORef, readIORef, writeIORef)
+import Data.List (isSuffixOf)
+import System.Directory
+    ( canonicalizePath
+    , doesDirectoryExist
+    , doesFileExist
+    , listDirectory
+    )
+import qualified Data.Set as Set (empty, insert, member)
+import Text.Printf (printf)
+
+dropSuffix :: String -> String -> String
+dropSuffix suffix s
+    | suffix `isSuffixOf` s = take (length s - length suffix) s
+    | otherwise = s
+
